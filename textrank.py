9b211873c4bebfe6290c932043398b940691faa3
Geoffrey Martin <Geoffrey@Geoffreys-MacBook-Pro.local>
2018-04-24 18:13:09 +0200

    more truncations to code

--- textrank.py
+++ textrank.py
@@ -7,6 +7,7 @@ sys.setdefaultencoding('utf-8')
 
 inputs = "ris.json"
 path_stage1 = "o1.json"
+path_stage2 = "o2.json"
 
 ris_string = json.load(open(inputs))
 pattern = re.compile("TI  - (.*?)\\r|AB  - (.*?)\\r")
@@ -24,24 +25,12 @@ with open('ris.json', 'w') as output:
 
 with open(path_stage1, 'w') as f:
     for graf in pytextrank.parse_doc(pytextrank.json_iter(inputs)):
-        pytextrank.pretty_print(graf._asdict())
         f.write("%s\n" % pytextrank.pretty_print(graf._asdict()))
 
-# In[]:
-
-path_stage1 = "o1.json"
-path_stage2 = "o2.json"
-
 graph, ranks = pytextrank.text_rank(path_stage1)
-pytextrank.render_ranks(graph, ranks)
 
 with open(path_stage2, 'w') as f:
     for rl in pytextrank.normalize_key_phrases(path_stage1, ranks):
         f.write("%s\n" % pytextrank.pretty_print(rl._asdict()))
 
-# In[]:
-
-path_stage2 = "o2.json"
-phrases = "\n".join(set([p for p in pytextrank.limit_keyphrases(path_stage2, phrase_limit=10)]))
-
-print 'keywords:\n' + phrases
+phrases = "\n".join(set([p for p in pytextrank.limit_keyphrases(path_stage2, phrase_limit=20)]))
